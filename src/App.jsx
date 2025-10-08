f23568765f5c04bc48ef1dd32a518c01a2b1574c
Nico <ngomez@hey.com>
2025-10-07 22:20:02 -0300

    Unidad 2 - Usar imágenes locales

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
 import Movie from './movies.jsx'
+import movie1 from './assets/movie-1.webp'
+import movie2 from './assets/movie-2.webp'
+import movie3 from './assets/movie-3.webp'
 
 function Header() {
   return (
@@ -18,17 +21,17 @@ function App() {
         <Movie
           name="Kimetsu no Yaiba: Fortaleza Infinita"
           description="El Cuerpo de Cazadores de Demonios se enfrenta a los Doce Kizuki restantes antes de enfrentarse a Muzan en el Castillo del Infinito para derrotarlo de una vez por todas."
-          image="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jl7e0hLYwIDsMlEQ28Gh30DoJRy.jpg"
+          image={movie1}
         />
         <Movie
           name="Expediente Warren: El último rito"
           description="Los investigadores de lo paranormal Ed y Lorraine Warren se enfrentan a un último caso aterrador en el que están implicadas entidades misteriosas a las que deben enfrentarse."
-          image="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vrfti9ycF4mlA5WuIyfgCf1LJQG.jpg"
+          image={movie2}
         />
         <Movie
           name="Una batalla tras otra"
           description="Cuando su principal enemigo resurge después de 16 años, una banda de ex revolucionarios se pone de nuevo en contacto para rescatar a la hija de uno de los suyos, encarnado por Leonardo DiCaprio. Adaptación de la novela 'Vineland', de Thomas Pynchon (1990), sobre los movimientos radicales de los años sesenta."
-          image="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zwxROLIUeEr6eFITwworjcXLAk4.jpg"
+          image={movie3}
         />
       </div>
     </>
