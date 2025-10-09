7304dec315505749d6c829e8c40bccc86a41e107
Nico <ngomez@hey.com>
2025-10-09 19:13:14 -0300

    Unidad 2 - Mostrar JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
 import Movie from './movies.jsx'
-import movie1 from './assets/movie-1.webp'
-import movie2 from './assets/movie-2.webp'
-import movie3 from './assets/movie-3.webp'
+import movies from './data/movies.json'
 
 function Header() {
   return (
@@ -18,21 +16,13 @@ function App() {
     <>
       <Header />
       <div className="movie-list">
-        <Movie
-          name="Kimetsu no Yaiba: Fortaleza Infinita"
-          description="El Cuerpo de Cazadores de Demonios se enfrenta a los Doce Kizuki restantes antes de enfrentarse a Muzan en el Castillo del Infinito para derrotarlo de una vez por todas."
-          image={movie1}
-        />
-        <Movie
-          name="Expediente Warren: El último rito"
-          description="Los investigadores de lo paranormal Ed y Lorraine Warren se enfrentan a un último caso aterrador en el que están implicadas entidades misteriosas a las que deben enfrentarse."
-          image={movie2}
-        />
-        <Movie
-          name="Una batalla tras otra"
-          description="Cuando su principal enemigo resurge después de 16 años, una banda de ex revolucionarios se pone de nuevo en contacto para rescatar a la hija de uno de los suyos, encarnado por Leonardo DiCaprio. Adaptación de la novela 'Vineland', de Thomas Pynchon (1990), sobre los movimientos radicales de los años sesenta."
-          image={movie3}
-        />
+        {movies.map(movie => (
+          <Movie
+            name={movie.title}
+            description={movie.overview}
+            image={movie.poster_path}
+          />
+        ))}
       </div>
     </>
   )
